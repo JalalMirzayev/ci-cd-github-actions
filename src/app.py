65cc5f6f4a2b0ac5f4ef6a5d203d0df223b5bcf2
jalalmirzayev <jalal.mirzayev@outlook.de>
2026-02-26 17:18:41 +0100

    fixed liniting and dependencies

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ A simple Python script that prints "Hello, World!" to the console.
 
 import numpy
 
+
 def hello():
     """
     Returns a greeting message.
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,8 +5,9 @@ Test suite for the app module.
 
 from src.app import hello
 
+
 def test_hello():
     """
     Test for the hello function in the app module.
     """
-    assert hello() == 'Hello, World!'
+    assert hello() == "Hello, World!"
