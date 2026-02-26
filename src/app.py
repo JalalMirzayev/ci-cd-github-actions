512e622493091ef7df9e31610ae34a5eab964ebb
jalalmirzayev <jalal.mirzayev@outlook.de>
2026-02-26 16:07:13 +0100

    initial commit

---
+++ src/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A simple Python script that prints "Hello, World!" to the console.
+"""
+
+def hello():
+    """
+    Returns a greeting message.
+    """
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    print(hello())
---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Test suite for the app module.
+"""
+
+from src.app import hello
+
+def test_hello():
+    """
+    Test for the hello function in the app module.
+    """
+    assert hello() == 'Hello, World!'
