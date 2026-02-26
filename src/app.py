1e51fd7b497b892439a91aa4d2b6ebf2cd14ec49
jalalmirzayev <jalal.mirzayev@outlook.de>
2026-02-26 17:07:13 +0100

    fixed liniting and dependencies

--- src/app.py
+++ src/app.py
@@ -3,11 +3,13 @@
 A simple Python script that prints "Hello, World!" to the console.
 """
 
+import numpy
+
 def hello():
     """
     Returns a greeting message.
     """
-    return "Hello, World!"
+    return "Hello, World!" + " " + str(numpy.array([1, 2, 3]))
 
 
 if __name__ == "__main__":
